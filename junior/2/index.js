66529410129e83b5eb6c3b4f4231ca4f753e6693
AhitkinIlya <43231077+AhitkinIlya@users.noreply.github.com>
2019-08-11 19:36:56 +0300

    junior # 2 is complete

--- junior/2/index.js
+++ junior/2/index.js
@@ -14,6 +14,7 @@
 // "(}" => False
 // "[(])" => False
 // "[({})] (]" => False
+// "{[)}"
 
 const input = document.getElementById('inputField');
 const button = document.getElementById('buttonClick');
@@ -26,35 +27,77 @@ button.addEventListener('click', () => {
 
 
 function findCorrectBracket(input) {
-  let count1 = 0;
-  let count2 = 0;
-  let count3 = 0
-  const text = input.split('')
-  console.log(text)
+  const text1 = input.replace(/\s/g, "").split('')
+  const text2 = input.replace(/\s/g, "").split('')
+  const text3 = input.replace(/\s/g, "").split('')
+  if (checkBrackets(text1, "[", "]") && checkBrackets(text2, "{", "}") && checkBrackets(text3, "(", ")")) {
+    return 'Верное расположение скобок'
+  }
 
-  for(let i = 0; i < text.length; i++) {
-    switch(text[i]) {
-      case '[':
-      case ']':
-        ++count1
-        break
-      case '{':
-      case '}':
-        ++count2
-        break
-      case '(':
-      case ')':
-        ++count3
-        break
-      default:
-        break
+  return 'Некорректное расположение скобок'
+
+}
+
+function checkBrackets(array, symbolOne, symbolTwo) {
+  let a = true
+  let bracketOne = 0
+  let bracketTwo = 0
+  let count = 0
+   while (a) {
+    bracketOne = array.indexOf(symbolOne)
+    bracketTwo = array.indexOf(symbolTwo)
+
+    if (bracketOne === -1 && bracketTwo === -1) {
+      return true
+    }
+
+    if (bracketOne !== -1 && bracketTwo !== -1) {
+      ++count
+      if (bracketOne < bracketTwo && (bracketTwo - bracketOne) % 2 !== 0) {
+        array.splice(bracketOne - 1, bracketTwo - bracketOne)
+      } else {
+        return false
+      }
+    } else if (count === 0) {
+      return false
+    } else {
+      return true
     }
   }
 
-  if (count1 % 2 !== 0 || count2 % 2 !== 0 || count3 % 2 !== 0) {
-    return 'false'
-  }
-
-
-  return 'true'
 }
+
+// function checkDistanceBetweenBrackets(array, ) {
+
+// }
+
+// function checkEven(array) {
+//   let count1 = 0;
+//   let count2 = 0;
+//   let count3 = 0;
+
+//   for(let i = 0; i < array.length; i++) {
+//     switch(array[i]) {
+//       case '[':
+//       case ']':
+//         ++count1
+//         break
+//       case '{':
+//       case '}':
+//         ++count2
+//         break
+//       case '(':
+//       case ')':
+//         ++count3
+//         break
+//       default:
+//         break
+//     }
+//   }
+
+//   if (count1 % 2 !== 0 || count2 % 2 !== 0 || count3 % 2 !== 0) {
+//     return false
+//   }
+
+//   return true
+// }
